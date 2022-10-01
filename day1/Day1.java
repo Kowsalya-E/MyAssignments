7f5842a3ade6068446868065b6056d63357389ec
Kowsalya-E <114809300+Kowsalya-E@users.noreply.github.com>
2022-10-01 16:34:27 +0530

    Add files via upload

---
+++ day1/Day1.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   double dou=45.7878787;
+   float float1=34.33f;
+   long long1=89739893;
+   int a=566;
+   boolean b=true;
+   String str="test";
+   char arr='c';
+   short sh=5647;
+   byte byte1=22;
+	}
+
+}
